ec2e109fd735a908dd748d746dec0d664c8b21ab
Pierre Langlois <pierre.langlois@epitech.eu>
2021-10-15 15:39:07 +0200

    [ADD] route management

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+import 'route_generator.dart';
+
+void main() {
+  runApp(Manga());
+}
+
+class Manga extends StatelessWidget {
+  const Manga({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      onGenerateRoute: RouteGenerator.generateRoute,
+    );
+  }
+}
---
+++ lib/route_generator.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'core/errors/error_route.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    // ignore: unused_local_variable
+    final args = settings.arguments; // To pass argument to another page
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => HomePage());
+    }
+    return errorRoute();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
