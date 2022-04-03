4ad5711ed6f763357ddf76f27dbd93baa6672328
Pierre Langlois <pierrelgs.pro@hotmail.com>
2022-04-03 15:02:31 +0200

    [WIP] start to dev the bar

--- lib/core/component/appbar.dart
+++ lib/core/component/appbar.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class MangaAppBar extends StatelessWidget with PreferredSizeWidget {
@@ -10,10 +9,66 @@ class MangaAppBar extends StatelessWidget with PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(title, style: Theme.of(context).textTheme.headline1),
-    );    
+      actions: [
+        IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () => {
+                  showSearch(
+                      context: context, delegate: MangaSearchDelelegate())
+                  // Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => SearchPage()))
+                })
+      ],
+    );
   }
 
   @override
   Size get preferredSize => Size.fromHeight(60);
-  
+}
+
+class MangaSearchDelelegate extends SearchDelegate {
+  @override
+  List<Widget>? buildActions(BuildContext context) => [IconButton(
+    onPressed: () {
+      query = '';
+    }, icon: Icon(Icons.clear))];
+
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () => close(context, null));
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Center(child: Text(query, style: TextStyle(color: Colors.white),));
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = List.empty();
+
+    return ListView.builder(itemCount: suggestions.length, itemBuilder: (context, index) {
+      return Text(suggestions[index]);
+    });
+  }
+}
+
+class SearchPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ResearchAppBar(),
+    );
+  }
+}
+
+class ResearchAppBar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      automaticallyImplyLeading: true,
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(60);
 }
--- lib/features/homepage/presentation/bloc/homepage_bloc.dart
+++ lib/features/homepage/presentation/bloc/homepage_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:mobile/features/homepage/domain/entities/MangaInfo.dart';
 import 'package:mobile/features/homepage/domain/usecases/get_homepage_scans.dart';
 
