77689baf241d840c4527ff3b67e8b47b872654cb
Pierre Langlois <pierrelgs.pro@hotmail.com>
2022-06-06 19:38:43 +0200

    [EDIT] change the model of mangaInfo

--- lib/data/model/manga_info_model.dart
+++ lib/data/model/manga_info_model.dart
@@ -1,3 +1,4 @@
+import '../../domain/entities/authorData.dart';
 import '../../domain/entities/manga_info.dart';
 
 class MangaInfoModel extends MangaInfo {
@@ -5,17 +6,19 @@ class MangaInfoModel extends MangaInfo {
     required String img,
     required String url,
     required String name,
-  }) : super(img: img, url: url, name: name);
+    required Author author,
+  }) : super(img: img, url: url, name: name, author: author);
 
   factory MangaInfoModel.fromJson(Map<String, dynamic> json) {
+    print(json);
     return MangaInfoModel(
-      url: json['url'],
-      img: json['img'],
-      name: json['name'],
+      url: json['url'] ?? '',
+      img: json['img'] ?? '',
+      name: json['name'] ?? '', author: Author.fromJson(json['author'] ?? Author.empty()),
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {'img': img, 'name': name, 'url': url};
+    return {'img': img, 'name': name, 'url': url, 'author': {'label': author.label}};
   }
 }
--- lib/domain/entities/manga_info.dart
+++ lib/domain/entities/manga_info.dart
@@ -1,24 +1,26 @@
 import 'package:equatable/equatable.dart';
+import 'package:mobile/domain/entities/authorData.dart';
 
 class MangaInfo extends Equatable {
   final String img; //urlimg
   final String name; //urlimg
   final String url; //urlimg
+  final Author author; //urlimg
 
-  MangaInfo(
-      {required this.img,
+  MangaInfo( 
+      {required this.author, required this.img,
       required this.name, required this.url});
 
   static final empty = MangaInfo(
     img: '',
     url: '',
-    name: '',
+    name: '', author: Author.empty(),
   );
 
-  MangaInfo copyWith({String? img, String? url, String? name}) {
-    return MangaInfo(img: img ?? this.img, url: url ?? this.url, name: name ?? this.name);
+  MangaInfo copyWith({String? img, String? url, String? name, Author? author}) {
+    return MangaInfo(img: img ?? this.img, url: url ?? this.url, name: name ?? this.name, author: author ?? this.author);
   }
 
   @override
-  List<Object?> get props => [img, url, name];
+  List<Object?> get props => [img, url, name, author];
 }
